9c1c1092aa4f0a73d747cf2c7d3984e441ed4e48
RR-Ardhika <rr.ardhika@gmail.com>
2024-02-10 18:22:58 +0700

    Adjust header content size

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div>
-      <h1 className="p-12 bg-teal text-9xl text-white">Murojaah</h1>
+      <h1 className="p-4 bg-teal text-4xl text-white">Murojaah</h1>
     </div>
   );
 }
