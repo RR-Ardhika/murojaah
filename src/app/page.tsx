578ba0d4652562e1f26df2fd46382ac08553f4e5
RR-Ardhika <rr.ardhika@gmail.com>
2024-02-10 22:02:45 +0700

    Use function for repeated mockup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,11 @@
+function repeat() {
+  const items = [];
+  for (let i = 0; i < 60; ++i) {
+    items.push(<p>This is content {i}</p>);
+  }
+  return items;
+}
+
 export default function Home() {
-  return (
-    <div className="mt-[72px] p-4">
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-      <p>This is content</p>
-    </div>
-  );
+  return <div className="mt-[72px] p-4">{repeat()}</div>;
 }
