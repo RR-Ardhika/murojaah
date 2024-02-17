3ca9e2815f722ca450eb436e1397e14aa20e8ed3
RR-Ardhika <rr.ardhika@gmail.com>
2024-02-17 15:24:48 +0700

    Remove alert close button

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -4,11 +4,7 @@ import { useContext } from 'react';
 import { AlertContext } from '@/context/AlertContext';
 
 export const Alert = (): JSX.Element => {
-  const { showAlert, setShowAlert } = useContext(AlertContext);
-
-  function hideAlert(): void {
-    setShowAlert(false);
-  }
+  const { showAlert } = useContext(AlertContext);
 
   return (
     <>
@@ -16,9 +12,6 @@ export const Alert = (): JSX.Element => {
         <div className="fixed w-full mt-[72px] p-2 bg-green-500 text-white text">
           <div className="flex justify-between">
             <p>Successfully created new murojaah</p>
-            <button className="w-4 h-6 border border-white" onClick={hideAlert}>
-              X
-            </button>
           </div>
         </div>
       )}
