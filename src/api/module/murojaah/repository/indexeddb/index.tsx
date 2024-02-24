59f91481db11c3f42b7e1e01a78fde48f28d91de
RR-Ardhika <rr.ardhika@gmail.com>
2024-02-24 23:14:39 +0700

    Add order by id desc

--- src/api/module/murojaah/repository/indexeddb/index.tsx
+++ src/api/module/murojaah/repository/indexeddb/index.tsx
@@ -2,14 +2,9 @@ import { idbCon } from '@/api/database/connection/indexeddb';
 import { History } from '@/api/module/murojaah/entity/murojaah';
 
 export function FindAll(): History[] {
-  return idbCon.select<History>({
-    from: 'histories',
-  });
+  return idbCon.select<History>({ from: 'histories', order: { by: 'id', type: 'desc' } });
 }
 
 export function Insert(history: History) {
-  return idbCon.insert({
-    into: 'histories',
-    values: [history],
-  });
+  return idbCon.insert({ into: 'histories', values: [history] });
 }
