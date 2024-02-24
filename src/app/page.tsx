4bca7c5804b4fd1ba1ea10cd161ca0d1524e5eaf
RR-Ardhika <rr.ardhika@gmail.com>
2024-02-25 01:23:45 +0700

    Fix jsstore raise terminal console error because it executed in ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 'use client';
 
-import { initJsStore } from '@/api/database/indexeddb/connection';
+import { DataProvider } from '@/context/DataContext';
 import { AlertProvider } from '@/context/AlertContext';
 import { Alert } from '@/components/Alert';
 import { HistoricalView } from '@/components/HistoricalView';
 import { CreateButton } from '@/components/CreateButton';
 
 const Home = (): JSX.Element => {
-  initJsStore();
+  if (typeof window === 'undefined') return <></>;
 
   return (
-    <AlertProvider>
-      <Alert />
-      <HistoricalView />
-      <CreateButton />
-    </AlertProvider>
+    <DataProvider>
+      <AlertProvider>
+        <Alert />
+        <HistoricalView />
+        <CreateButton />
+      </AlertProvider>
+    </DataProvider>
   );
 };
 
