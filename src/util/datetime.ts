16c6dace217cd44350c7bdf7c1e455ea98bd237e
RR-Ardhika <rr.ardhika@gmail.com>
2024-02-25 21:12:04 +0700

    Fix string function return value

--- src/util/datetime.ts
+++ src/util/datetime.ts
@@ -2,7 +2,7 @@ import { DateTime } from 'luxon';
 
 export function formatDatetime(date: Date): string {
   const parsedTime: DateTime = DateTime.fromJSDate(date);
-  if (!parsedTime.isValid) return undefined;
+  if (!parsedTime.isValid) return '';
   const front: string = parsedTime.toFormat('EEE, MMM dd ');
   const back: string = parsedTime.toFormat('yy hh:mm a');
   return front + "'" + back;
