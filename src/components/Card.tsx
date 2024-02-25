f881eac3db1832da0c839f0c6f4d6e42a013316e
RR-Ardhika <rr.ardhika@gmail.com>
2024-02-25 08:47:37 +0700

    Add buttons mockup

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { History, MurojaahType } from '@/api/module/murojaah/entity';
 import { formatDatetime } from '@/util/datetime';
+import { clsx } from 'clsx';
 
 export const Card = (item: History): JSX.Element => {
   const cardClassnames: Record<string, string> = {
@@ -10,12 +12,32 @@ export const Card = (item: History): JSX.Element => {
   };
 
   const JuzCard = (): JSX.Element => {
+    const btnClass: Record<string, string> = {
+      base: 'p-2 rounded',
+      edit: 'bg-yellow-500 hover:bg-yellow-700',
+      delete: 'bg-red-500 hover:bg-red-700',
+    };
+
+    const [isButtonsVisible, setIsButtonsVisible] = useState(false);
+
+    function toggleButtons(): void {
+      isButtonsVisible ? setIsButtonsVisible(false) : setIsButtonsVisible(true);
+    }
+
     return (
       <div className={cardClassnames.container}>
-        <p className={cardClassnames.title}>Juz {item.juz}</p>
-        <p className={cardClassnames.data}>Murojaah by using {item.murojaahMethod}</p>
-        <p className={cardClassnames.data}>Total Murojaah is {item.totalMurojaah}</p>
-        <p className={cardClassnames.date}>{formatDatetime(item.occuredAt)}</p>
+        <div onClick={toggleButtons}>
+          <p className={cardClassnames.title}>Juz {item.juz}</p>
+          <p className={cardClassnames.data}>Murojaah by using {item.murojaahMethod}</p>
+          <p className={cardClassnames.data}>Total Murojaah is {item.totalMurojaah}</p>
+          <p className={cardClassnames.date}>{formatDatetime(item.occuredAt)}</p>
+        </div>
+        {isButtonsVisible && (
+          <div className="flex flex-col gap-2 w-full mt-2">
+            <button className={clsx(btnClass.base, btnClass.edit)}>Edit</button>
+            <button className={clsx(btnClass.base, btnClass.delete)}>Delete</button>
+          </div>
+        )}
       </div>
     );
   };
