d38b1658088e4cd8d8e882504f6fb5b1f9306221
RR-Ardhika <rr.ardhika@gmail.com>
2024-03-12 12:00:59 +0700

    Add more data in mockup

--- src/app/stat/page.tsx
+++ src/app/stat/page.tsx
@@ -1,10 +1,14 @@
+import { clsx } from 'clsx';
+
 const Stat = (): JSX.Element => {
   const classNames: Record<string, string> = {
     container: 'gap-[20px] mt-[72px] pt-4 px-4',
-    card: 'px-4 pt-4 bg-custom-teal text-white rounded-lg',
+    card: 'p-4 bg-custom-teal text-white rounded-lg',
+    marginTop: 'mt-4',
     title: 'text-xl font-black',
-    dataContainer: 'grid grid-cols-3 grid-rows-3 gap-0',
-    data: 'col-span-2',
+    dataContainer: 'grid grid-cols-12 gap-x-2 items-center',
+    fieldNameCol: 'col-span-9',
+    fieldValueCol: 'col-span-2',
   };
 
   return (
@@ -13,37 +17,65 @@ const Stat = (): JSX.Element => {
         <p className={classNames.title}>All Times</p>
         <hr />
         <div className={classNames.dataContainer}>
-          <p>Total lines: </p>
-          <p className={classNames.data}>100</p>
-          <p>Total juz:</p>
-          <p className={classNames.data}>1</p>
+          <p className={classNames.fieldNameCol}>Total lines read</p>
+          <p>:</p>
+          <p className={classNames.fieldValueCol}>100</p>
+
+          <p className={classNames.fieldNameCol}>Total juz done from lines</p>
+          <p>:</p>
+          <p className={classNames.fieldValueCol}>1</p>
+
+          <p className={classNames.fieldNameCol}>Total marked juz as done</p>
+          <p>:</p>
+          <p className={classNames.fieldValueCol}>1</p>
         </div>
 
-        <p className={classNames.title}>Daily</p>
+        <p className={clsx(classNames.marginTop, classNames.title)}>Daily</p>
         <hr />
         <div className={classNames.dataContainer}>
-          <p>Total lines: </p>
-          <p className={classNames.data}>100</p>
-          <p>Total juz:</p>
-          <p className={classNames.data}>1</p>
+          <p className={classNames.fieldNameCol}>Total lines read</p>
+          <p>:</p>
+          <p>100</p>
+
+          <p className={classNames.fieldNameCol}>Total juz done from lines</p>
+          <p>:</p>
+          <p>1</p>
+
+          <p className={classNames.fieldNameCol}>Total marked juz as done</p>
+          <p>:</p>
+          <p>1</p>
         </div>
 
-        <p className={classNames.title}>Weekly</p>
+        <p className={clsx(classNames.marginTop, classNames.title)}>Weekly</p>
         <hr />
         <div className={classNames.dataContainer}>
-          <p>Total lines: </p>
-          <p className={classNames.data}>100</p>
-          <p>Total juz:</p>
-          <p className={classNames.data}>1</p>
+          <p className={classNames.fieldNameCol}>Total lines read</p>
+          <p>:</p>
+          <p>100</p>
+
+          <p className={classNames.fieldNameCol}>Total juz done from lines</p>
+          <p>:</p>
+          <p>1</p>
+
+          <p className={classNames.fieldNameCol}>Total marked juz as done</p>
+          <p>:</p>
+          <p>1</p>
         </div>
 
-        <p className={classNames.title}>Monthly</p>
+        <p className={clsx(classNames.marginTop, classNames.title)}>Monthly</p>
         <hr />
         <div className={classNames.dataContainer}>
-          <p>Total lines: </p>
-          <p className={classNames.data}>100</p>
-          <p>Total juz:</p>
-          <p className={classNames.data}>1</p>
+          <p className={classNames.fieldNameCol}>Total lines read</p>
+          <p>:</p>
+          <p>100</p>
+
+          <p className={classNames.fieldNameCol}>Total juz done from lines</p>
+          <p>:</p>
+          <p>1</p>
+
+          <p className={classNames.fieldNameCol}>Total marked juz as done</p>
+          <p>:</p>
+          <p>1</p>
         </div>
       </div>
     </div>
