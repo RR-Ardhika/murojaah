149ea8fe8126218120273efc6884bb28bbe5f616
RR-Ardhika <rr.ardhika@gmail.com>
2024-03-12 11:33:34 +0700

    Add mockup

--- src/app/stat/page.tsx
+++ src/app/stat/page.tsx
@@ -1,6 +1,6 @@
 const Stat = (): JSX.Element => {
   const classNames: Record<string, string> = {
-    container: 'mt-[72px] pt-4 px-4',
+    container: 'gap-[20px] mt-[72px] pt-4 px-4',
     card: 'px-4 pt-4 bg-custom-teal text-white rounded-lg',
     title: 'text-xl font-black',
     dataContainer: 'grid grid-cols-3 grid-rows-3 gap-0',
@@ -10,7 +10,35 @@ const Stat = (): JSX.Element => {
   return (
     <div className={classNames.container}>
       <div className={classNames.card}>
+        <p className={classNames.title}>All Times</p>
+        <hr />
+        <div className={classNames.dataContainer}>
+          <p>Total lines: </p>
+          <p className={classNames.data}>100</p>
+          <p>Total juz:</p>
+          <p className={classNames.data}>1</p>
+        </div>
+
         <p className={classNames.title}>Daily</p>
+        <hr />
+        <div className={classNames.dataContainer}>
+          <p>Total lines: </p>
+          <p className={classNames.data}>100</p>
+          <p>Total juz:</p>
+          <p className={classNames.data}>1</p>
+        </div>
+
+        <p className={classNames.title}>Weekly</p>
+        <hr />
+        <div className={classNames.dataContainer}>
+          <p>Total lines: </p>
+          <p className={classNames.data}>100</p>
+          <p>Total juz:</p>
+          <p className={classNames.data}>1</p>
+        </div>
+
+        <p className={classNames.title}>Monthly</p>
+        <hr />
         <div className={classNames.dataContainer}>
           <p>Total lines: </p>
           <p className={classNames.data}>100</p>
