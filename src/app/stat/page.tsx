d1439aff7b412a2ba7a779f9baeeaf486e8c56c4
RR-Ardhika <rr.ardhika@gmail.com>
2024-03-12 10:32:15 +0700

    Replace hello world

--- src/app/stat/page.tsx
+++ src/app/stat/page.tsx
@@ -1,5 +1,25 @@
 const Stat = (): JSX.Element => {
-  return <div className="mt-20">Hello, World!!!</div>;
+  const classNames: Record<string, string> = {
+    container: 'mt-[72px] pt-4 px-4',
+    card: 'px-4 pt-4 bg-custom-teal text-white rounded-lg',
+    title: 'text-xl font-black',
+    dataContainer: 'grid grid-cols-3 grid-rows-3 gap-0',
+    data: 'col-span-2',
+  };
+
+  return (
+    <div className={classNames.container}>
+      <div className={classNames.card}>
+        <p className={classNames.title}>Daily</p>
+        <div className={classNames.dataContainer}>
+          <p>Total lines: </p>
+          <p className={classNames.data}>100</p>
+          <p>Total juz:</p>
+          <p className={classNames.data}>1</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stat;
