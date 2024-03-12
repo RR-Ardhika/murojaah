180f8e108800b7bfdb5a1142b1a300987c0a5a57
RR-Ardhika <rr.ardhika@gmail.com>
2024-03-12 09:06:36 +0700

    Add routing

---
+++ src/app/stat/page.tsx
@@ -0,0 +1,5 @@
+const Stat = (): JSX.Element => {
+  return <div className="mt-20">Hello, World!!!</div>;
+};
+
+export default Stat;
--- src/web/shared/component/Navbar.tsx
+++ src/web/shared/component/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export const Navbar = (): JSX.Element => {
   const navClass: Record<string, string> = {
     container: 'flex mt-auto sticky bottom-0 border-t border-t-white',
@@ -6,9 +8,18 @@ export const Navbar = (): JSX.Element => {
 
   return (
     <nav className={navClass.container}>
-      <div className={navClass.div}>H</div>
-      <div className={navClass.div}>A</div>
-      <div className={navClass.div}>C</div>
+      <Link href="/" className={navClass.div}>
+        H
+      </Link>
+      <Link href="#" className={navClass.div}>
+        A
+      </Link>
+      <Link href="#" className={navClass.div}>
+        C
+      </Link>
+      <Link href="/stat" className={navClass.div}>
+        S
+      </Link>
     </nav>
   );
 };
