c6a6c516712638574a7f34aed5f2d95fc65a6498
RR-Ardhika <rr.ardhika@gmail.com>
2024-03-13 20:14:33 +0700

    Refactor to View component

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,47 +1,7 @@
-import { Stat } from '@/api/module/stat/entity';
-import { Card } from '@/web/module/stat/component/Card';
+import { View } from '@/web/module/stat/component/View';
 
-const Stat = (): JSX.Element => {
-  // TODO Remove dummy data
-  const dummy: Stat[] = [
-    {
-      id: 1,
-      statType: 0,
-      totalLinesRead: 100,
-      totalJuzFromLines: 1.5,
-      totalMarkedJuzAsDone: 1,
-    },
-    {
-      id: 2,
-      statType: 1,
-      totalLinesRead: 100,
-      totalJuzFromLines: 1.5,
-      totalMarkedJuzAsDone: 1,
-    },
-    {
-      id: 3,
-      statType: 2,
-      totalLinesRead: 100,
-      totalJuzFromLines: 1.5,
-      totalMarkedJuzAsDone: 1,
-    },
-    {
-      id: 4,
-      statType: 4,
-      totalLinesRead: 100,
-      totalJuzFromLines: 1.5,
-      totalMarkedJuzAsDone: 1,
-    },
-  ];
-
-  return (
-    <div className="gap-[20px] mt-[72px] pt-4 px-4">
-      <div className="flex flex-col gap-4 p-4 bg-custom-teal text-white rounded-lg">
-        {/* TODO Remove dummy data */}
-        {dummy ? dummy.map((item: Stat) => <Card key={item.id} {...item} />) : <></>}
-      </div>
-    </div>
-  );
+const Page = (): JSX.Element => {
+  return <View />;
 };
 
-export default Stat;
+export default Page;
---
+++ src/web/module/stat/component/View.tsx
@@ -0,0 +1,45 @@
+import { Stat } from '@/api/module/stat/entity';
+import { Card } from '@/web/module/stat/component/Card';
+
+export const View = (): JSX.Element => {
+  // TODO Remove dummy data
+  const dummy: Stat[] = [
+    {
+      id: 1,
+      statType: 0,
+      totalLinesRead: 100,
+      totalJuzFromLines: 1.5,
+      totalMarkedJuzAsDone: 1,
+    },
+    {
+      id: 2,
+      statType: 1,
+      totalLinesRead: 100,
+      totalJuzFromLines: 1.5,
+      totalMarkedJuzAsDone: 1,
+    },
+    {
+      id: 3,
+      statType: 2,
+      totalLinesRead: 100,
+      totalJuzFromLines: 1.5,
+      totalMarkedJuzAsDone: 1,
+    },
+    {
+      id: 4,
+      statType: 4,
+      totalLinesRead: 100,
+      totalJuzFromLines: 1.5,
+      totalMarkedJuzAsDone: 1,
+    },
+  ];
+
+  return (
+    <div className="gap-[20px] mt-[72px] pt-4 px-4">
+      <div className="flex flex-col gap-4 p-4 bg-custom-teal text-white rounded-lg">
+        {/* TODO Remove dummy data */}
+        {dummy ? dummy.map((item: Stat) => <Card key={item.id} {...item} />) : <></>}
+      </div>
+    </div>
+  );
+};
