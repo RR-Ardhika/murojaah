a67d5ec450514a426fa9a984f9e115076ec7bdbd
RR-Ardhika <rr.ardhika@gmail.com>
2024-09-15 22:48:55 +0700

    Add three line menu button

--- src/web/shared/component/Header.tsx
+++ src/web/shared/component/Header.tsx
@@ -1,7 +1,26 @@
 const Header = (): JSX.Element => {
   return (
     <header className="fixed w-full p-4 bg-custom-teal text-4xl text-white border-b border-b-white">
-      Murojaah
+      <div className="flex justify-between">
+        <p>Murojaah</p>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            // @ts-expect-error expected type
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </div>
     </header>
   );
 };
