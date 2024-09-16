ea7222e50c41ec550ea8e6d0e033dcd7a7368f49
RR-Ardhika <rr.ardhika@gmail.com>
2024-09-16 16:25:57 +0700

    Refactor to Menu shared component

--- src/web/shared/component/Header.tsx
+++ src/web/shared/component/Header.tsx
@@ -1,25 +1,11 @@
+import Menu from '@/web/shared/component/Menu';
+
 const Header = (): JSX.Element => {
   return (
     <header className="fixed w-full p-4 bg-custom-teal text-4xl text-white border-b border-b-white">
       <div className="flex justify-between">
         <p>Murojaah</p>
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            // @ts-expect-error expected type
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
+        <Menu />
       </div>
     </header>
   );
