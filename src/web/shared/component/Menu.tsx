5722b8424d21ba5db2b9d7713a8f3cf00470072b
RR-Ardhika <rr.ardhika@gmail.com>
2024-09-16 23:19:54 +0700

    Replace three line menu with headlessui

--- src/web/shared/component/Menu.tsx
+++ src/web/shared/component/Menu.tsx
@@ -1,22 +1,28 @@
-const Menu = (): JSX.Element => {
-  return (
-    <button>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M4 6h16M4 12h16M4 18h16"
-        />
-      </svg>
-    </button>
-  );
-};
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { Bars3Icon, ArrowUpTrayIcon, ArrowDownTrayIcon } from '@heroicons/react/16/solid';
 
-export default Menu;
+export default function Component(): JSX.Element {
+  return (
+    <Menu>
+      <MenuButton>
+        <Bars3Icon className="size-6" />
+      </MenuButton>
+
+      <MenuItems anchor="bottom end" className="w-52">
+        <MenuItem>
+          <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 bg-custom-teal text-white">
+            <ArrowUpTrayIcon className="size-4 fill-white/50" />
+            Export
+          </button>
+        </MenuItem>
+
+        <MenuItem>
+          <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 bg-custom-teal text-white">
+            <ArrowDownTrayIcon className="size-4 fill-white/50" />
+            Import
+          </button>
+        </MenuItem>
+      </MenuItems>
+    </Menu>
+  );
+}
