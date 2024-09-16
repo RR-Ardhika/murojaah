e17277f2d800d74fd2fcbc3712d0fcf2e9c27a84
RR-Ardhika <rr.ardhika@gmail.com>
2024-09-16 22:44:18 +0700

    Fix svg properties

--- src/web/shared/component/Menu.tsx
+++ src/web/shared/component/Menu.tsx
@@ -3,16 +3,15 @@ const Menu = (): JSX.Element => {
     <button>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        // @ts-expect-error expected type
-        class="h-6 w-6"
+        className="h-6 w-6"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
           d="M4 6h16M4 12h16M4 18h16"
         />
       </svg>
