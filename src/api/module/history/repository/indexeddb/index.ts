a68afa02383f02bc41e32b94b87b243978716ac7
RR-Ardhika <rr.ardhika@gmail.com>
2024-10-06 16:58:57 +0700

    Add export PoC

--- src/api/module/history/repository/indexeddb/index.ts
+++ src/api/module/history/repository/indexeddb/index.ts
@@ -1,11 +1,13 @@
 import * as entity from '@/api/module/history/entity';
 import { Connection } from 'jsstore';
 import { initJsStore } from '@/api/database/indexeddb/connection';
+import Dexie from 'dexie';
+// @ts-expect-error valid import
+import * as IDBExportImport from 'indexeddb-export-import';
 
 const idbCon: Connection = initJsStore();
 
 export function FindAll(): Promise<unknown> {
-  // TD-4 Implement order by date
   return idbCon.select<History>({ from: 'histories', order: { by: 'occuredAt', type: 'desc' } });
 }
 
@@ -21,8 +23,28 @@ export function DeleteRecord(item: entity.History): Promise<unknown> {
   });
 }
 
-export function Export(): void {
-  console.log('export');
+export async function Export(): Promise<unknown> {
+  const db: Dexie = new Dexie('murojaah');
+  db.version(1).stores({
+    histories:
+      'id, historyType, juz, surah, surahName, startAyah, endAyah, markSurahDone, markJuzDone, approachId, repeat, occuredAt',
+  });
+  try {
+    await db.open();
+    const idbDatabase: IDBDatabase = db.backendDB(); // Get native IndexedDB Database object
+    IDBExportImport.exportToJsonString(idbDatabase, (err: Error, jsonString: string) => {
+      if (err) {
+        console.error('Export failed:', err);
+        return err;
+      }
+
+      console.log('Exported JSON:', jsonString);
+      return jsonString;
+    });
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
 }
 
 export function Import(): void {
