41203db0cf080197876bdfe237645bc044e77744
RR-Ardhika <rr.ardhika@gmail.com>
2024-10-27 15:26:29 +0700

    Add different bg color for menu and store it to classNames

--- src/web/shared/component/Menu.tsx
+++ src/web/shared/component/Menu.tsx
@@ -7,6 +7,13 @@ import { Export, Import } from '@/api/module/history/service';
 import Links from '@/web/shared/util/const';
 
 export default function Component(): JSX.Element {
+  const classNames: Record<string, string> = {
+    menuItems:
+      'w-52 origin-top-right rounded-xl border border-white/20 bg-teal-700 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0',
+    menuItem:
+      '"group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 bg-teal-700 data-[focus]:bg-teal-800 text-white"',
+  };
+
   const fileInputRef: React.RefObject<HTMLInputElement> = useRef<HTMLInputElement | null>(null);
 
   // @ts-expect-error expected return value type
@@ -64,25 +71,16 @@ export default function Component(): JSX.Element {
           <Bars3Icon className="size-6" />
         </MenuButton>
 
-        <MenuItems
-          anchor="bottom end"
-          className="w-52 origin-top-right rounded-xl border border-white/20 bg-custom-teal p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
-        >
+        <MenuItems anchor="bottom end" className={classNames.menuItems}>
           <MenuItem>
-            <button
-              className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 bg-custom-teal data-[focus]:bg-teal-700 text-white"
-              onClick={doExport}
-            >
+            <button className={classNames.menuItem} onClick={doExport}>
               <ArrowUpTrayIcon className="size-4 fill-white/50" />
               Export
             </button>
           </MenuItem>
 
           <MenuItem>
-            <button
-              className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 bg-custom-teal data-[focus]:bg-teal-700 text-white"
-              onClick={doImport}
-            >
+            <button className={classNames.menuItem} onClick={doImport}>
               <ArrowDownTrayIcon className="size-4 fill-white/50" />
               Import
             </button>
@@ -91,11 +89,7 @@ export default function Component(): JSX.Element {
           <div className="my-1 h-px bg-white/20" />
 
           <MenuItem>
-            <a
-              className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 bg-custom-teal data-[focus]:bg-teal-700 text-white"
-              href={Links.GitHub}
-              target="_blank"
-            >
+            <a className={classNames.menuItem} href={Links.GitHub} target="_blank">
               <img className="w-4 h-4" src="/github-mark-white.svg" alt="icon" />
               GitHub
             </a>
