ec434d02717f515184a67023fa3c1371c5419e35
RR-Ardhika <rr.ardhika@gmail.com>
2024-11-11 00:13:23 +0700

    Add rule for naming convention

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,25 +32,53 @@ module.exports = {
       },
     ],
     '@typescript-eslint/explicit-function-return-type': 'error',
-    // '@typescript-eslint/naming-convention': [
-    //   'error',
-    //   {
-    //     selector: 'variable',
-    //     format: ['camelCase'],
-    //   },
-    //   {
-    //     selector: 'function',
-    //     format: ['camelCase'],
-    //   },
-    //   {
-    //     selector: 'class',
-    //     format: ['PascalCase'],
-    //   },
-    //   {
-    //     selector: 'typeLike',
-    //     format: ['PascalCase'],
-    //   },
-    // ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      // Variables: camelCase, UPPER_CASE for constants
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+      },
+      // Functions: camelCase
+      {
+        selector: 'function',
+        format: ['camelCase'],
+      },
+      // Classes, interfaces, and types: PascalCase
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      // Enum members: PascalCase
+      {
+        selector: 'enumMember',
+        format: ['PascalCase'],
+      },
+      // Properties and methods: camelCase, leading underscores allowed for private members
+      {
+        selector: 'property',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+      },
+      {
+        selector: 'method',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+      },
+      // Static members: camelCase or UPPER_CASE
+      {
+        selector: 'property',
+        modifiers: ['static'],
+        format: ['camelCase', 'UPPER_CASE'],
+      },
+      // Type parameters: PascalCase, prefixed with T
+      {
+        selector: 'typeParameter',
+        format: ['PascalCase'],
+        prefix: ['T'],
+      },
+    ],
     '@typescript-eslint/typedef': [
       'error',
       {
@@ -65,4 +93,19 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['**/component/**/*.{ts,tsx}'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'variable',
+            format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+            leadingUnderscore: 'allow',
+          },
+        ],
+      },
+    },
+  ],
 };
