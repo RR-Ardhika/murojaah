4e1788a72ea0f623555d9db49d6aab3570349564
RR-Ardhika <rr.ardhika@gmail.com>
2024-11-11 00:27:59 +0700

    Update rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,25 +13,8 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   rules: {
-    'react-hooks/exhaustive-deps': 'error',
-    '@typescript-eslint/explicit-module-boundary-types': 'error',
-    'import/order': [
-      'error',
-      {
-        groups: [
-          'builtin', // Node "builtin" modules (like fs, path)
-          'external', // External packages (like react, lodash)
-          'internal', // Internal modules (within the project)
-          ['parent', 'sibling', 'index'], // Parent, sibling, and index imports
-        ],
-        'newlines-between': 'always', // Adds newlines between groups
-        alphabetize: {
-          order: 'asc', // Sorts within groups alphabetically
-          caseInsensitive: true,
-        },
-      },
-    ],
     '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
     '@typescript-eslint/naming-convention': [
       'error',
       // Variables: camelCase, UPPER_CASE for constants
@@ -92,6 +75,25 @@ module.exports = {
         variableDeclarationIgnoreFunction: true,
       },
     ],
+    // 'func-style': ['error', 'expression', { allowArrowFunctions: true }],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin', // Node "builtin" modules (like fs, path)
+          'external', // External packages (like react, lodash)
+          'internal', // Internal modules (within the project)
+          ['parent', 'sibling', 'index'], // Parent, sibling, and index imports
+        ],
+        'newlines-between': 'always', // Adds newlines between groups
+        alphabetize: {
+          order: 'asc', // Sorts within groups alphabetically
+          caseInsensitive: true,
+        },
+      },
+    ],
+    // 'prefer-arrow-callback': 'error',
+    'react-hooks/exhaustive-deps': 'error',
   },
   overrides: [
     {
