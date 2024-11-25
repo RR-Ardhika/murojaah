31fd7d4d59d269189b3eab879d64748ca7285c30
RR-Ardhika <rr.ardhika@gmail.com>
2024-11-25 20:59:55 +0700

    Implement multiple render detection in web history module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,20 @@ import { CreateButton } from '@/web/module/history/component/CreateButton';
 import { View } from '@/web/module/history/component/View';
 import { DataProvider } from '@/web/module/history/context/DataContext';
 import { Alert } from '@/web/shared/component/Alert';
+import { Base } from '@/web/shared/component/Base';
 import { AlertProvider } from '@/web/shared/context/AlertContext';
 
 const Page = (): JSX.Element => {
   return (
-    <DataProvider>
-      <AlertProvider>
-        <Alert />
-        <View />
-        <CreateButton />
-      </AlertProvider>
-    </DataProvider>
+    <Base module="history" name="Page">
+      <DataProvider>
+        <AlertProvider>
+          <Alert />
+          <View />
+          <CreateButton />
+        </AlertProvider>
+      </DataProvider>
+    </Base>
   );
 };
 
