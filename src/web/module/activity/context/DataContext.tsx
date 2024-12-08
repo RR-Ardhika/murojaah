752b49aea01d6e02e2d086beb31e742c1fe2ba7e
RR-Ardhika <rr.ardhika@gmail.com>
2024-12-08 16:48:18 +0700

    Fix multiple render in module activity

--- src/web/module/activity/context/DataContext.tsx
+++ src/web/module/activity/context/DataContext.tsx
@@ -1,11 +1,11 @@
 import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useState } from 'react';
 
-import * as entity from '@/api/module/history/entity';
-import * as service from '@/api/module/history/service';
+import * as entity from '@/api/module/activity/entity';
+import * as service from '@/api/module/activity/service';
 import { Base } from '@/web/shared/component/Base';
 
 interface InternalProps {
-  setData: Dispatch<SetStateAction<entity.History[] | undefined>>;
+  setData: Dispatch<SetStateAction<entity.Activity[] | undefined>>;
 }
 
 // @ts-expect-error DataContextValues
@@ -20,11 +20,11 @@ export const useData = (): Context<DataContextValues> => {
 };
 
 const fetchData = (i: InternalProps): void => {
-  service.index().then((result: entity.History[]) => i.setData(result));
+  service.index().then((result: entity.Activity[]) => i.setData(result));
 };
 
 export const DataProvider = ({ children }: { children: ReactNode }): JSX.Element => {
-  const [data, setData] = useState<entity.History[]>();
+  const [data, setData] = useState<entity.Activity[]>();
 
   const i: InternalProps = {
     setData,
