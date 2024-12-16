0ab197079fc016a04b52ad9f856cadc8ff097871
RR-Ardhika <rr.ardhika@gmail.com>
2024-12-16 09:27:56 +0700

    Refactor links and navbar

--- src/shared/const/index.ts
+++ src/shared/const/index.ts
@@ -1,7 +1,7 @@
 export const LINKS: Record<string, string> = {
   HOME: '/',
-  HISTORY_COMPACT_DATE: '/histories?view=compact-date',
-  HISTORY_LIST_SURAH: '/histories?view=list-surah',
+  ACTIVITY_COMPACT_DATE: '/activities?view=compact-date',
+  ACTIVITY_LIST_SURAH: '/activities?view=list-surah',
   STAT: '/stats',
   GITHUB: 'https://github.com/RR-Ardhika/murojaah',
 } as const;
