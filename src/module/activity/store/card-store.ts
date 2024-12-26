4b4c71078597243e2c38a5d8dc10a1740cbfb7a9
RR-Ardhika <rr.ardhika@gmail.com>
2024-12-26 10:47:42 +0700

    Fix build error

--- src/module/activity/store/card-store.ts
+++ src/module/activity/store/card-store.ts
@@ -2,11 +2,11 @@ import { create } from 'zustand';
 
 interface CardState {
   expandedCardId: string | null;
-  setExpandedCardId: (id: string | null) => void;
+  setExpandedCardId: (value: string | null) => void;
 }
 
 // eslint-disable-next-line @typescript-eslint/typedef
 export const useCardStore = create<CardState>()((set) => ({
   expandedCardId: null,
-  setExpandedCardId: (id: string | null) => set({ expandedCardId: id }),
+  setExpandedCardId: (value: string | null): void => set({ expandedCardId: value }),
 }));
