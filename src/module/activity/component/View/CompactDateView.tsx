19e55de29d7f2c9811a1b3e350f6e82587564797
RR-Ardhika <rr.ardhika@gmail.com>
2024-12-28 17:21:16 +0700

    Add striped style

--- src/module/activity/component/View/CompactDateView.tsx
+++ src/module/activity/component/View/CompactDateView.tsx
@@ -1,3 +1,4 @@
+import { clsx } from 'clsx';
 import { useEffect } from 'react';
 
 import { Base } from '@/shared/component/Base';
@@ -8,6 +9,12 @@ import { useCompactDateDataStore } from '../../store';
 const CLASS_NAMES: Record<string, string> = {
   container: 'flex justify-between',
   content: 'text-lg text-custom-teal',
+  striped: 'bg-gray-100',
+};
+
+const addStripedClassNames = (i: number): string => {
+  if (i % 2 === 0) return CLASS_NAMES.container;
+  return clsx(CLASS_NAMES.container, CLASS_NAMES.striped);
 };
 
 export const CompactDateView = (): React.JSX.Element => {
@@ -22,9 +29,9 @@ export const CompactDateView = (): React.JSX.Element => {
     <Base module="activity" name="CompactDateView">
       <div className="flex flex-col pt-4 px-4 mt-[72px]">
         {data &&
-          data.map((item: CompactDate) => {
+          data.map((item: CompactDate, i: number) => {
             return (
-              <div key={item.date} className={CLASS_NAMES.container}>
+              <div key={item.date} className={addStripedClassNames(i)}>
                 <p className={CLASS_NAMES.content}>{item.date}</p>
                 <p className={CLASS_NAMES.content}>{item.stat.juz} juz</p>
               </div>
