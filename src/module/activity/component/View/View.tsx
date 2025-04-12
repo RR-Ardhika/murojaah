1c8eee13dc32dca170186178b5018e1104c2e6bf
RR-Ardhika <rr.ardhika@gmail.com>
2025-04-12 21:01:48 +0700

    Improve alert positioning

--- src/module/activity/component/View/View.tsx
+++ src/module/activity/component/View/View.tsx
@@ -22,7 +22,7 @@ export const View = (): React.JSX.Element => {
 
   return (
     <Base module="activity" name="View">
-      <div className={clsx('flex flex-col pt-4 px-4', isAlertVisible ? 'mt-[112px]' : 'mt-[72px]')}>
+      <div className="flex flex-col pt-4 px-4 mt-[72px]">
         {data &&
           data.map((group: ActivityGroup) => {
             return (
