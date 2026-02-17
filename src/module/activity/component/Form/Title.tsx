5497393cf148098754eba9d37e067f7aa79d1674
Ardhika <rr.ardhika@gmail.com>
2026-02-17 16:09:58 +0700

    fix(activity): wrong form description (#71)

--- src/module/activity/component/Form/Title.tsx
+++ src/module/activity/component/Form/Title.tsx
@@ -1,13 +1,16 @@
 import { Dialog } from '@headlessui/react';
 
+import { activityTypeLabel, ActivityType } from '../../entity';
 import { useFormStore } from '../../store';
 
 export const Title = (): React.JSX.Element => {
-  const { formType } = useFormStore();
+  const { formType, activity } = useFormStore();
+  const label: string = activityTypeLabel[formType as ActivityType] ?? '';
+  const action: string = activity ? 'Update' : 'Create';
 
   return (
     <Dialog.Title className="text-lg font-medium leading-6 text-gray-900">
-      Create {formType} Murojaah
+      {action} {label} Murojaah
     </Dialog.Title>
   );
 };
